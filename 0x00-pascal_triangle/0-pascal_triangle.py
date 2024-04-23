53c2b8b1f222211eca283a25dee1c03364652996
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-04-23 20:34:58 +0200

    Added task "0. Pascal's Triangle"
    Create a function 'def pascal_triangle(n):' that returns a list of lists of integers representing the Pascal’s triangle of 'n':
    
    - Returns an empty list if 'n <= 0'
    - Assume 'n' will be always an integer
    
    ```
    guillaume@ubuntu:~/0x00$ cat 0-main.py
    #!/usr/bin/python3
    """
    0-main
    """
    pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
    
    def print_triangle(triangle):
        """
        Print the triangle
        """
        for row in triangle:
            print("[{}]".format(",".join([str(x) for x in row])))
    
    if __name__ == "__main__":
        print_triangle(pascal_triangle(5))
    
    guillaume@ubuntu:~/0x00$
    guillaume@ubuntu:~/0x00$ ./0-main.py
    [1]
    [1,1]
    [1,2,1]
    [1,3,3,1]
    [1,4,6,4,1]
    guillaume@ubuntu:~/0x00$
    ```

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,45 @@
+""" 0-pascal_triangle.py
+
+This module is aimed to solve the pascal triangle problem.
+"""
+
+def binomial_coefficient(n, k):
+    """Binomial Coefficient
+    
+    Arguements:
+        n (int): The row to be calculated.
+        k (int): The column to be calculated.
+    
+    Return:
+        (int): The number yeilded by nCm
+    """
+    if k == 0 or k == n:
+        return 1
+    else:
+        return binomial_coefficient(n - 1, k - 1) + binomial_coefficient(n - 1, k)
+    
+def pascal_triangle(n):
+    """Pascal Triangle
+
+    Arguments:
+        n (int): The height of the pascal triangle to be returned
+
+    Return:
+        (list): The flattened pascal triangle of height n
+    """
+    if type(n) is not int:
+        raise TypeError("n must be an integer")
+    elif n <= 0:
+        return []
+
+    pTriangle = []
+    for row in range(n):
+        rowlist = []
+        for col in range(row + 1):
+            if col == 0 or col == row:
+                rowlist.append(1)
+            else:
+                rowlist.append(binomial_coefficient(row, col))
+        pTriangle.append(rowlist)
+    
+    return pTriangle
