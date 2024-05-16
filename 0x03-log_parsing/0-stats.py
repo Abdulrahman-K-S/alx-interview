7337da34de6489c17619602bb061b0ee8098274a
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-05-17 01:34:34 +0300

    Added task "0. Log parsing"
    Write a script that reads `stdin` line by line and computes metrics:
    
    - Input format: `<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>` (if the format is not this one, the line must be skipped)
    - After every 10 lines and/or a keyboard interruption (`CTRL + C`), print these statistics from the beginning:
     - Total file size: `File size: <total size>`
     - where `<total size>` is the sum of all previous `<file size>` (see input format above)
     - Number of lines by status code:
      - possible status code: `200`, `301`, `400`, `401`, `403`, `404`, `405` and `500`
      - if a status code doesn’t appear or is not an integer, don’t print anything for this status code
      - format: `<status code>: <number>`
      - status codes should be printed in ascending order
    
    **Warning:** In this sample, you will have random value - it’s normal to not have the same output as this one.
    
    ```py
    alexa@ubuntu:~/0x03-log_parsing$ cat 0-generator.py
    #!/usr/bin/python3
    import random
    import sys
    from time import sleep
    import datetime
    
    for i in range(10000):
        sleep(random.random())
        sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
            random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
            datetime.datetime.now(),
            random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
            random.randint(1, 1024)
        ))
        sys.stdout.flush()
    
    alexa@ubuntu:~/0x03-log_parsing$ ./0-generator.py | ./0-stats.py
    File size: 5213
    200: 2
    401: 1
    403: 2
    404: 1
    405: 1
    500: 3
    File size: 11320
    200: 3
    301: 2
    400: 1
    401: 2
    403: 3
    404: 4
    405: 2
    500: 3
    File size: 16305
    200: 3
    301: 3
    400: 4
    401: 2
    403: 5
    404: 5
    405: 4
    500: 4
    ^CFile size: 17146
    200: 4
    301: 3
    400: 4
    401: 2
    403: 6
    404: 6
    405: 4
    500: 4
    Traceback (most recent call last):
      File "./0-stats.py", line 15, in <module>
    Traceback (most recent call last):
      File "./0-generator.py", line 8, in <module>
        for line in sys.stdin:
    KeyboardInterrupt
        sleep(random.random())
    KeyboardInterrupt
    alexa@ubuntu:~/0x03-log_parsing$
    ```
    
    Files:
    - 0-stats.py

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+Task 0. Log parsing
+
+A script that reads stdin line by line and computes metrics.
+"""
+
+import sys
+
+
+def printStats(file_size, status):
+    """printStats
+
+    This function takes the total file size and the
+    statues that were called and prints them.
+
+    Arguments:
+        file_size (int): The total file size to be printed.
+        status (dict{int, int}): A dictionary of the statues that were called.
+    """
+    print("File size: {}".format(file_size))
+    for key, value in sorted(status.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
+
+
+if __name__ == "__main__":
+    total_file_size = 0
+    count = 0
+    possible_status = {200: 0, 301: 0, 400: 0, 401: 0,
+                       403: 0, 404: 0, 405: 0, 500: 0}
+
+    try:
+        for line in sys.stdin:
+            args = line.split()
+
+            if len(args) > 2:
+                status_code = int(args[-2])
+                file_size = int(args[-1])
+
+                if status_code in possible_status:
+                    possible_status[status_code] += 1
+
+                total_file_size += file_size
+                count += 1
+
+                if count == 10:
+                    printStats(total_file_size, possible_status)
+                    count = 0
+    except KeyboardInterrupt:
+        pass
+    finally:
+        printStats(total_file_size, possible_status)
