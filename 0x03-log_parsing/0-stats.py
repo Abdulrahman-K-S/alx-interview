ca501fe030457f6024dc9b66b3cc0208eda82d01
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-05-17 01:39:12 +0300

    Update 0-stats.py
    Trying to remove the int() for the status code

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -34,7 +34,7 @@ try:
         args = line.split()
 
         if len(args) > 2:
-            status_code = int(args[-2])
+            status_code = args[-2]
             file_size = int(args[-1])
 
             if status_code in possible_status:
