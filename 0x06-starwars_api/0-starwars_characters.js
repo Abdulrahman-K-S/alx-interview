f92651bc1ca9237995b00f92e3831c506ca5e6c0
Abdulrahman Khaled <AK-Salah@outlook.com>
2024-06-06 08:33:57 -0700

    (fix): Fix semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,23 +3,22 @@
  * A script that prints all characters of a Star Wars movie
 */
 
-
-const request = require('request')
-const filmNum = process.argv[2] + '/'
-const filmURL = 'https://swapi-api.alx-tools.com/api/films/'
+const request = require('request');
+const filmNum = process.argv[2] + '/';
+const filmURL = 'https://swapi-api.alx-tools.com/api/films/';
 
 request(filmURL + filmNum, async (err, res, body) => {
-    if (err) return console.error(err);
+  if (err) return console.error(err);
 
-    const charactersURLList = JSON.parse(body).characters
-    for (const chracterURL of charactersURLList) {
-        await new Promise((resolve, reject) => {
-            request(chracterURL, (err, res, body) => {
-                if (err) return console.error(err);
+  const charactersURLList = JSON.parse(body).characters;
+  for (const chracterURL of charactersURLList) {
+    await new Promise((resolve, reject) => {
+      request(chracterURL, (err, res, body) => {
+        if (err) return console.error(err);
 
-                console.log(JSON.parse(body).name);
-                resolve();
-            });
-        });
-    }
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
 });
