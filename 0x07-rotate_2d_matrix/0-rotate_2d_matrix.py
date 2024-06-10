efb6625c4ed0c34e6c2470624998a6e7da4e5ad8
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-06-10 20:33:24 +0300

    (feat): Add task "0. Rotate 2D Matrix"
    Given an `n` x `n` 2D matrix, rotate it 90 degrees clockwise.
    
    - Prototype: `def rotate_2d_matrix(matrix):`
    - Do not return anything. The matrix must be edited **in-place**.
    - You can assume the matrix will have 2 dimensions and will not be empty.
    
    ```py
    jessevhedden$ cat main_0.py
    #!/usr/bin/python3
    """
    Test 0x07 - Rotate 2D Matrix
    """
    rotate_2d_matrix = __import__('0-rotate_2d_matrix').rotate_2d_matrix
    
    if __name__ == "__main__":
        matrix = [[1, 2, 3],
                  [4, 5, 6],
                  [7, 8, 9]]
    
        rotate_2d_matrix(matrix)
        print(matrix)
    
    jessevhedden$
    jessevhedden$ ./main_0.py
    [[7, 4, 1],
    [8, 5, 2],
    [9, 6, 3]]
    jessevhedden$
    ```
    
    File:
    - 0-rotate_2d_matrix.py

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Task 0. Rotate 2D Matrix
+
+Rotate an n by n matrix
+"""
+
+from typing import List
+
+
+def rotate_2d_matrix(matrix: List):
+    """rotate_2d_matrix
+
+    Takes an n by n matrix and rotates the matrix by
+    90* clockwise.
+
+    Arguments:
+        matrix (List[n][n]): The matrix to rotate by 90*.
+    """
+    size = len(matrix)
+    temp = [[x for x in row] for row in matrix]
+    for i in range(size):
+        for j in range(size):
+            matrix[j][size-1-i] = temp[i][j]
