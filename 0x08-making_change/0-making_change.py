f8d72642d23c27099636fa66a1850d67d729a005
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-06-20 17:43:11 +0300

    Feat: Add task "0. Change comes from within"

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Task 0. Change comes from within
+
+Determine the fewest number of coins needed to meet a given amount total.
+"""
+
+
+def makeChange(coins, total):
+    """makeChange
+
+    Given a pile of coins of different values, determine the fewest
+    number of coins needed to meet a given amount total.
+
+    Arguments:
+        coins (List): A list (pile) of coins of different values.
+        total (int): The total needed.
+
+    Return:
+        (int): The minimum number of coins needed to meet the total. 
+    """
+    temp_value = 0
+    coins.sort(reverse=True)
+
+    if total <= 0:
+        return 0
+
+    for coin in coins:
+        if total % coin <= total:
+            temp_value += total // coin
+            total = total % coin
+
+    return temp_value if total == 0 else -1
